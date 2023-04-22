55e7ab4a6621aa5bee84e0b5336a5fea9304d4ed
Robert <robertschauss91@gmail.com>
2023-04-22 14:09:52 -0700

    created basic code for the pages, still needs some tweaks and JSX updates

--- client/src/Pages/DailyLogPage.js
+++ client/src/Pages/DailyLogPage.js
@@ -1,8 +1,33 @@
 import React, { useState } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
+import DailyLogForm from '../components/DailyLogs/DailyLogForm';
+import DailyLogItem from '../components/DailyLogs/DailyLogItem';
+import DailyLogList from '../components/DailyLogs/DailyLogList';
+
+const DisplayLogPage = () => {
+  const [dailyLogs, setDailyLogs] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
+  const handleDailyLogSubmit = (dailyLog) => {
+    setDailyLogs([...dailyLogs, dailyLog]);
+    setShowForm(false);
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>Daily Log Page</h1>
+      <DailyLogList dailyLogs={dailyLogs} />
+      <button
+        onClick={() => setShowForm(true)}
+        className="btn btn-primary mt-3"
+        >Create Form
+      </button>
+      {showForm && <DailyLogForm onSubmit={handleDailyLogSubmit} />}
+      <DailyLogItem />
+    </div>
+  );
+};
+
+export default DisplayLogPage;
--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,8 +1,55 @@
 import React, { useState } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
+// import {
+//   Container,
+//   Card,
+//   Button,
+//   Row,
+//   Col
+// } from 'react-bootstrap';
+// will use later for finaly JSX
+
+import { LoginForm } from '../components/Users/LoginForm';
+import { RegisterForm } from '../components/Users/RegisterForm';
+import { UserProfile } from '../components/Users/UserProfile';
+// import {}
+
+const HomePage = () => {
+  const [result, setResult] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false); // State variable to track logged in status
+
+  const handleButtonClick = (option) => {
+    // Logic to handle button click based on option
+    if (option === 'login') {
+      setResult('Login button clicked');
+      setLoggedIn(true); // Set logged in status to true
+    } else if (option === 'register') {
+      setResult('Register button clicked');
+      setLoggedIn(true); // Set logged in status to true
+    } else if (option === 'profile') {
+      setResult('User Profile button clicked');
+      setLoggedIn(true); // Set logged in status to true
+    }
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {loggedIn ? (
+        <div>
+          <UserProfile />
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => handleButtonClick('login')}>Login</button>
+          <LoginForm />
+          <button onClick={() => handleButtonClick('register')}>Register New User</button>
+          <RegisterForm />
+          {/* <button onClick={() => handleButtonClick('profile')}>User Profile</button> */}
+        </div>
+      )}
+      {/* {result && <p>Result: {result}</p>} */}
+    </div>
+  );
+};
+
+export default HomePage;
