4a766a32dbf81d6636b4bc48200407aae5e0d98e
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-04-23 21:37:14 -0700

    reverting old changes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
