7b0efbfe8492fb6526c0a1aeb16c767172272e38
Robert <robertschauss91@gmail.com>
2023-04-27 20:36:31 -0700

    worked on App.js and Nav.js formatting and JSX

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProjectPage from './pages/ProjectPage';
 import DailyLogPage from './pages/DailyLogPage';
-import {UserProfile,LoginForm, RegisterForm} from './components/Users/';
-
-
+import {UserProfile, LoginForm, RegisterForm } from './components/Users';
 import {
   ApolloClient,
   InMemoryCache,
@@ -13,9 +11,11 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import ProjectList from './components/Projects/ProjectList';
-
+// import ProjectList from './components/Projects/ProjectList';
+import { Container, Grid, Header } from '@mantine/core';
+// import { Row, Col } from '@mantine/core';
 import MyNav from './components/Navigation/Nav';
+// import { Link } from "react-router-dom";
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -36,20 +36,59 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const links = [
+  { label: "Home", link: "/" },
+  { label: "Projects", link: "/projects" },
+  { label: "Daily Log", link: "/dailylog" },
+  { label: "Login", link: "/login" },
+  { label: "Profile", link: "/profile" },
+  { label: "Signup", link: "/registerForm" },
+  
+  // Add other links here
+];
+
 const App = () => {
+  // const isSmallerThanLg = useMediaQuery("(max-width: 768px)");
   return (
     <ApolloProvider client={client}>
       <Router>
-        <MyNav />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/projects" element={<ProjectPage />} />
-          <Route path="/dailylog" element={<DailyLogPage />} />
-          <Route path="/login"element={<LoginForm/>}/>
-          <Route path="/signup"element={<RegisterForm/>}/>
-          <Route path="/profile" element={<UserProfile />} />
-          {/* Add other routes here */}
-        </Routes>
+        <Header
+        // {/* // style={{ paddingTop: isSmallerThanLg ? "1rem" : "5rem" }} */}
+        >
+          <Grid>
+            <Container>
+              <Grid.Col>
+                <MyNav
+                  // withBorder
+                  // position="fixed"
+                  // activeLinkClassName="text-blue-600 font-bold"
+                  link={links.map(({ label, link }) => (
+                    <Link key={link} to={link}>
+                      {label}
+                    </Link>
+                  ))}
+                />
+              </Grid.Col>
+            </Container>
+            </Grid>
+            </Header>
+            <Container>
+              <Grid>
+            {/* <Value> */}
+              <Grid.Col xs={12}>
+                <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/projects" element={<ProjectPage />} />
+                  <Route path="/dailylog" element={<DailyLogPage />} />
+                  <Route path="/login" element={<LoginForm />} />
+                  <Route path="/profile" element={<UserProfile />} />
+                  <Route path="/signup"element={<RegisterForm/>}/>
+                  {/* Add other routes here */}
+                </Routes>
+              </Grid.Col>
+            {/* </Value> */}
+          </Grid>
+        </Container>
       </Router>
       {/* main */}
     </ApolloProvider>
@@ -57,5 +96,3 @@ const App = () => {
 };
 
 export default App;
-
-
