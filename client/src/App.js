c7e1d6125187b7edbc3e27bd4864ff9e67520cc2
Robert <robertschauss91@gmail.com>
2023-04-28 19:30:37 -0700

    Changes to Nav and App styling and links.

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ProjectPage from './pages/ProjectPage';
-import DailyLogPage from './pages/DailyLogPage';
-import {UserProfile, LoginForm, RegisterForm } from './components/Users';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProjectPage from "./pages/ProjectPage";
+import DailyLogPage from "./pages/DailyLogPage";
+import { UserProfile, LoginForm, RegisterForm } from "./components/Users";
+import MyNav from "./components/Navigation/Nav";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-// import ProjectList from './components/Projects/ProjectList';
-import { Container, Grid, Header } from '@mantine/core';
-// import { Row, Col } from '@mantine/core';
-import MyNav from './components/Navigation/Nav';
-// import { Link } from "react-router-dom";
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Container, Row, Col, Navbar } from "react-bootstrap";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -43,54 +40,36 @@ const links = [
   { label: "Login", link: "/login" },
   { label: "Profile", link: "/profile" },
   { label: "Signup", link: "/registerForm" },
-  
-  // Add other links here
 ];
 
 const App = () => {
-  // const isSmallerThanLg = useMediaQuery("(max-width: 768px)");
   return (
     <ApolloProvider client={client}>
       <Router>
-        <Header
-        // {/* // style={{ paddingTop: isSmallerThanLg ? "1rem" : "5rem" }} */}
-        >
-          <Grid>
-            <Container>
-              <Grid.Col>
-                <MyNav
-                  // withBorder
-                  // position="fixed"
-                  // activeLinkClassName="text-blue-600 font-bold"
-                  link={links.map(({ label, link }) => (
-                    <Link key={link} to={link}>
-                      {label}
-                    </Link>
-                  ))}
-                />
-              </Grid.Col>
-            </Container>
-            </Grid>
-            </Header>
-            <Container>
-              <Grid>
-            {/* <Value> */}
-              <Grid.Col xs={12}>
-                <Routes>
-                  <Route path="/" element={<HomePage />} />
-                  <Route path="/projects" element={<ProjectPage />} />
-                  <Route path="/dailylog" element={<DailyLogPage />} />
-                  <Route path="/login" element={<LoginForm />} />
-                  <Route path="/profile" element={<UserProfile />} />
-                  <Route path="/signup"element={<RegisterForm/>}/>
-                  {/* Add other routes here */}
-                </Routes>
-              </Grid.Col>
-            {/* </Value> */}
-          </Grid>
+        <Navbar>
+          <MyNav
+            link={links.map(({ label, link }) => (
+              <Link key={link} to={link}>
+                {label}
+              </Link>
+            ))}
+          />
+        </Navbar>
+        <Container>
+          <Row>
+            <Col>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/projects" element={<ProjectPage />} />
+                <Route path="/dailylog" element={<DailyLogPage />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/profile" element={<UserProfile />} />
+                <Route path="/signup" element={<RegisterForm />} />
+              </Routes>
+            </Col>
+          </Row>
         </Container>
       </Router>
-      {/* main */}
     </ApolloProvider>
   );
 };
