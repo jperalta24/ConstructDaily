b95a243b02ac908e493d723364f9d4443a10180a
Brianna Ligon <desmondsaelee@hotmail.com>
2023-05-01 21:54:49 -0700

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/constructDaily"
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/ConstructDaily');
 
 module.exports = mongoose.connection;
