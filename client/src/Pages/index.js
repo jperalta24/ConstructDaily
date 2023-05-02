7dba4d83ed3b3d1eda19fa7f0c5245d31e6c1aa3
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-05-01 18:20:24 -0700

    new issues

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,3 +1,4 @@
-export { default as HomePage } from "./HomePage";
+
+export {default as HomePage} from "./HomePages"
 export { default as DailyLogPage } from "./DailyLogPage";
 export { default as ProjectPage } from "./ProjectPage";
