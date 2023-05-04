d8a6be684bc0aa89fd490151ff948254671f434b
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-05-03 19:38:56 -0700

    send help

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/constructDaily"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/constructDaily", {
+    newUrlParser: true,
+    useUnifiedTopology: true
+  }
 );
 
 module.exports = mongoose.connection;
