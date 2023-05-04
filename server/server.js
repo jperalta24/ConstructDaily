4312bce22c98e8750e1a5083e103c223e4526683
Slender James <114044205+jperalta24@users.noreply.github.com>
2023-05-03 19:29:10 -0700

    fixing server.js

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 // Create a new instance of an Apollo server with the GraphQL schema
